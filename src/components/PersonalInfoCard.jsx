7896b6cb446afe1ecb3b049f440261a838d04c82
Ehsan Ehrari <ehsan.ehrari@codetoinspire.org>
2025-12-22 11:51:36 +0430

    Add PersonalInfoCard and Row components
    Introduces PersonalInfoCard for displaying user info and a reusable Row component for rendering label-value pairs.

---
+++ src/components/PersonalInfoCard.jsx
@@ -0,0 +1,16 @@
+import Row from './Row'
+export default function PersonalInfoCard({info}){
+    return (
+        <div className="card infoCard">
+            <h2 className="cardTitle">Personal Information</h2>
+
+            <div className="rows">
+                {
+                    Object.keys(info).map((label)=>{
+                        <Row label={label} value={info[label]}></Row>
+                    })
+                }
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Row.jsx
@@ -0,0 +1,8 @@
+export default function Row({label, value}){
+    return (
+        <div className="row">
+            <span className="rowLabel"> {label} </span>
+            <span className="rowValue"> {value} </span>
+        </div>
+    )
+}
